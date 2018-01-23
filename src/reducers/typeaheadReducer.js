e07282a524b6dea12d3e314608798411b06c94d5
duhaime <douglas.duhaime@gmail.com>
2018-01-23 15:27:08 -0500

    limit expressed typeahead responses - closes #17

--- src/reducers/typeaheadReducer.js
+++ src/reducers/typeaheadReducer.js
@@ -10,28 +10,28 @@ const typeaheadReducer = (state = initialState, action) => {
   switch (action.type) {
     case 'SET_TYPEAHEAD_FIELD':
       return Object.assign({}, state, {
-        field: action.field
+        field: action.field,
       })
 
     case 'SET_TYPEAHEAD_QUERY':
       return Object.assign({}, state, {
-        query: action.query
+        query: action.query,
       })
 
     case 'SET_TYPEAHEAD_INDEX':
       return Object.assign({}, state, {
-        index: action.index
+        index: action.index,
       })  
 
     case 'RECEIVE_TYPEAHEAD_RESULTS':
       return Object.assign({}, state, {
         err: null,
-        results: action.results
+        results: action.results,
       })
 
     case 'TYPEAHEAD_REQUEST_FAILED':
       return Object.assign({}, state, {
-        err: true
+        err: true,
       })
 
     default:
