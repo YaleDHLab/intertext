8dc8fa2613a2e83a236a39eecc01a4896cfe98d4
Peter Leonard <pleonard@gmail.com>
2021-05-17 13:52:28 -0400

    add CUDA status

--- intertext/intertext.py
+++ intertext/intertext.py
@@ -212,6 +212,7 @@ def process_texts(**kwargs):
 
   # minhash files & store hashbands in db
   print(' * creating minhashes')
+  print('Using CUDA: ' + str(CUDA_AVAILABLE))
   get_all_hashbands(**kwargs)
 
   # find all hashbands that have multiple distict file_ids
