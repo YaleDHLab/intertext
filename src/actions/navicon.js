c1468a7fc92656265b7b563d4d79846b4a934cbc
duhaime <douglas.duhaime@gmail.com>
2021-06-21 10:49:26 -0400

    clear out v1

--- src/actions/navicon.js
+++
@@ -1,7 +0,0 @@
-export const open = () => ({
-  type: 'OPEN_NAVICON',
-})
-
-export const close = () => ({
-  type: 'CLOSE_NAVICON',
-})
--- src/components/Loader.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default class Loader extends React.Component {
-  render() {
-    return (
-      <div className='loader'></div>
-    )
-  }
-}
